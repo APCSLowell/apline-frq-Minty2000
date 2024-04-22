ae9a56920e0414d3e6bd5d9d01fa9965eb292889
Minty2000 <142567186+Minty2000@users.noreply.github.com>
2024-04-21 20:27:52 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,5 +1,17 @@
 public class APLine
 {
+  private int a,b,c;
   /* your code here */
-  
+  APLine(int a1,int b1,int c2){
+    a =a1; 
+    b= b2;
+    c=c2;
+  }
+
+  public getSlope(){
+    return -(double)(a)/b;
+  }
+  public isOnLine(int x, int y){
+    return a*x + b*y +c =0;
+  }
 }
